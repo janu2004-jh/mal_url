a7281d12c3419ac464639815d5355830f7b705f3
janu2004-jh <jahnavijh2004@gmail.com>
2024-12-17 20:17:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,5 +102,5 @@ def logout():
 
 # ================= Run the App ==================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Get port from environment or default to 5000
+    port = int(os.environ.get("PORT", 8080))  # Get port from environment or default to 5000
     app.run(debug=True, host="0.0.0.0", port=port)
